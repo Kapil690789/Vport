59e345cb10ed6b3dccaf5511ad5bb2dbf4c8e08b
Kapil Sharma <kapil19092003@gmail.com>
2024-10-18 13:08:13 +0530

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 p-4 text-center">
-      <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Kapil Sharma All rights reserved.</p>
     </footer>
   );
 };
